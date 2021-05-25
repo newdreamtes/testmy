84aed365f36744fc19fbf33e22a7124e7f3f17cf
newdreamtes <84498623+newdreamtes@users.noreply.github.com>
2021-05-25 17:59:38 +0530

    Create sql1.sql

---
+++ sql1.sql
@@ -0,0 +1,16 @@
+USE [sqlndsdb1]
+GO
+
+/****** Object:  Table [dbo].[test]    Script Date: 5/25/2021 5:56:39 PM ******/
+
+
+CREATE TABLE [dbo].[test2](
+	[id] [int] NULL,
+	[name] [varchar](100) NULL,
+	[description] [varchar](255) NULL
+)
+
+GO
+
+
+
