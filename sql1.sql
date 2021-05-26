1f597f92a63fa436d4c80a4b8677eb993fab9a81
newdreamtes <84498623+newdreamtes@users.noreply.github.com>
2021-05-26 12:06:05 +0530

    Update sql1.sql

--- sql1.sql
+++ sql1.sql
@@ -4,7 +4,7 @@ GO
 /****** Object:  Table [dbo].[test]    Script Date: 5/25/2021 5:56:39 PM ******/
 
 
-CREATE TABLE [dbo].[test2](
+CREATE TABLE [dbo].[test3](
 	[id] [int] NULL,
 	[name] [varchar](100) NULL,
 	[description] [varchar](255) NULL
